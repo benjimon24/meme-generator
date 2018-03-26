660e0dce59378bc209d997ddf6190ba5e918aa81
benjimon24 <benji102491@gmail.com>
2018-03-26 09:58:08 -0400

    add queries api and configured with docker

---
+++ queries-api/src/test/java/com/example/queriesapi/QueriesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.queriesapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueriesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
