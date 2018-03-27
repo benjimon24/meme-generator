607f3068151d83c234aaca2e9e75a7d2aec29858
benjimon24 <benji102491@gmail.com>
2018-03-27 00:00:01 -0400

    complete routes for get, post, and delete with tests

--- memes-api/src/main/java/com/example/memesapi/controllers/MemesController.java
+++ memes-api/src/main/java/com/example/memesapi/controllers/MemesController.java
@@ -1,13 +1,62 @@
 package com.example.memesapi.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.memesapi.models.Meme;
+import com.example.memesapi.repositories.MemeRepository;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class MemesController {
 
+    @Autowired
+    private MemeRepository memeRepository;
+
     @GetMapping("/")
-    public String findAllUsers() {
-        return "It's working!!!";
+    public Iterable<Meme> findAllUsers() {
+        return memeRepository.findAll();
+    }
+
+    @GetMapping("/{memeId}")
+    public Meme findMemeById(@PathVariable Long memeId) throws NotFoundException{
+        Meme foundMeme = memeRepository.findOne(memeId);
+
+        if (foundMeme == null) {
+            throw new NotFoundException("Meme with ID of " + memeId + " was not found!");
+        }
+
+        return foundMeme;
+    }
+
+    @PostMapping("/")
+    public Meme createNewMeme(@RequestBody Meme newMeme) {
+        return memeRepository.save(newMeme);
+    }
+
+    @DeleteMapping("/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) throws EmptyResultDataAccessException {
+        memeRepository.delete(userId);
+        return HttpStatus.OK;
+    }
+
+    @ExceptionHandler
+    void handleMemeNotFound(
+            NotFoundException exception,
+            HttpServletResponse response) throws IOException {
+
+        response.sendError(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    }
+
+    @ExceptionHandler
+    void handleDeleteNotFoundException(
+            EmptyResultDataAccessException exception,
+            HttpServletResponse response) throws IOException {
+
+        response.sendError(HttpStatus.NOT_FOUND.value());
     }
 }
--- memes-api/src/test/java/com/example/memesapi/controllers/MemesControllerTest.java
+++ memes-api/src/test/java/com/example/memesapi/controllers/MemesControllerTest.java
@@ -1,14 +1,19 @@
 package com.example.memesapi.controllers;
 
 
+import com.example.memesapi.models.Meme;
 import com.example.memesapi.repositories.MemeRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
 
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -34,7 +39,36 @@ public class MemesControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private MemeRepository mockUserRepository;
+    private MemeRepository mockMemeRepository;
+
+    private Meme newMeme;
+
+    @Autowired
+    private ObjectMapper jsonObjectMapper;
+
+    @Before
+    public void setUp() {
+        Meme firstMeme = new Meme(
+                "testing"
+        );
+
+        Meme secondMeme = new Meme(
+                "testing2"
+        );
+
+        Iterable<Meme> mockMemes =
+                Stream.of(firstMeme, secondMeme).collect(Collectors.toList());
+
+        given(mockMemeRepository.findAll()).willReturn(mockMemes);
+        given(mockMemeRepository.findOne(1L)).willReturn(firstMeme);
+        given(mockMemeRepository.findOne(4L)).willReturn(null);
+        doAnswer(invocation -> {
+            throw new EmptyResultDataAccessException("ERROR MESSAGE FROM MOCK!!!", 1234);
+        }).when(mockMemeRepository).delete(4L);
+
+        newMeme = new Meme("testing3");
+        given(mockMemeRepository.save(newMeme)).willReturn(newMeme);
+    }
 
     @Test
     public void findAllMemes_success_returnsStatusOK() throws Exception {
@@ -51,4 +85,93 @@ public class MemesControllerTest {
                 .perform(get("/"))
                 .andExpect(jsonPath("$", hasSize(2)));
     }
+
+    @Test
+    public void findAllMemes_success_returnUserNameForEachUser() throws Exception {
+
+        this.mockMvc
+                .perform(get("/"))
+                .andExpect(jsonPath("$[0].url", is("testing")));
+    }
+
+    @Test
+    public void findMemeById_success_returnsStatusOK() throws Exception {
+
+        this.mockMvc
+                .perform(get("/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void findMemeById_success_returnUrl() throws Exception {
+
+        this.mockMvc
+                .perform(get("/1"))
+                .andExpect(jsonPath("$.url", is("testing")));
+    }
+
+    @Test
+    public void findMemeById_failure_memeNotFoundReturns404() throws Exception {
+
+        this.mockMvc
+                .perform(get("/4"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void findMemeById_failure_memeNotFoundReturnsNotFoundErrorMessage() throws Exception {
+
+        this.mockMvc
+                .perform(get("/4"))
+                .andExpect(status().reason(containsString("Meme with ID of 4 was not found!")));
+    }
+
+    @Test
+    public void deleteMemeById_success_returnsStatusOk() throws Exception {
+
+        this.mockMvc
+                .perform(delete("/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteMemeById_success_deletesViaRepository() throws Exception {
+
+        this.mockMvc.perform(delete("/1"));
+
+        verify(mockMemeRepository, times(1)).delete(1L);
+    }
+
+    @Test
+    public void deleteMemeById_failure_userNotFoundReturns404() throws Exception {
+
+        this.mockMvc
+                .perform(delete("/4"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void createMeme_success_returnsStatusOk() throws Exception {
+
+        this.mockMvc
+                .perform(
+                        post("/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonObjectMapper.writeValueAsString(newMeme))
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void createMeme_success_returnsUrl() throws Exception {
+
+        this.mockMvc
+                .perform(
+                        post("/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(jsonObjectMapper.writeValueAsString(newMeme))
+                )
+                .andExpect(jsonPath("$.url", is("testing3")));
+    }
+
 }
